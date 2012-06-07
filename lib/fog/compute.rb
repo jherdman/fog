ec5db7377dec9c618a884d5d48ce04b125911181
James Herdman <james.herdman@me.com>
2012-06-06 23:57:09 -0400

    Use `autoload` for Compute modules

--- lib/fog/compute.rb
+++ lib/fog/compute.rb
@@ -1,6 +1,30 @@
 module Fog
   module Compute
 
+    autoload :AWS,            'fog/aws/compute'
+    autoload :Bluebox,        'fog/bluebox/compute'
+    autoload :Brightbox,      'fog/brightbox/compute'
+    autoload :Cloudstack,     'fog/cloudstack/compute'
+    autoload :Clodo,          'fog/clodo/compute'
+    autoload :Ecloud,         'fog/ecloud/compute'
+    autoload :Glesys,         'fog/glesys/compute'
+    autoload :GoGrid,         'fog/go_grid/compute'
+    autoload :HP,             'fog/hp/compute'
+    autoload :IBM,            'fog/ibm/compute'
+    autoload :Joyent,         'fog/joyent/compute'
+    autoload :Libvirt,        'fog/libvirt/compute'
+    autoload :Linode,         'fog/linode/compute'
+    autoload :BareMetalCloud, 'fog/bare_metal_cloud/compute'
+    autoload :Ninefold,       'fog/ninefold/compute'
+    autoload :OpenStack,      'fog/openstack/compute'
+    autoload :Ovirt,          'fog/ovirt/compute'
+    autoload :Rackspace,      'fog/rackspace/compute'
+    autoload :StormOnDemand,  'fog/storm_on_demand/compute'
+    autoload :Vcloud,         'fog/vcloud/compute'
+    autoload :Voxel,          'fog/voxel/compute'
+    autoload :Vsphere,        'fog/vsphere/compute'
+    autoload :XenServer,      'fog/xenserver/compute'
+
     def self.[](provider)
       self.new(:provider => provider)
     end
@@ -9,84 +33,58 @@ module Fog
       attributes = attributes.dup # prevent delete from having side effects
       case provider = attributes.delete(:provider).to_s.downcase.to_sym
       when :aws
-        require 'fog/aws/compute'
-        Fog::Compute::AWS.new(attributes)
+        AWS.new(attributes)
       when :bluebox
-        require 'fog/bluebox/compute'
-        Fog::Compute::Bluebox.new(attributes)
+        Bluebox.new(attributes)
       when :brightbox
-        require 'fog/brightbox/compute'
-        Fog::Compute::Brightbox.new(attributes)
+        Brightbox.new(attributes)
       when :cloudstack
-        require 'fog/cloudstack/compute'
-        Fog::Compute::Cloudstack.new(attributes)
+        Cloudstack.new(attributes)
       when :clodo
-        require 'fog/clodo/compute'
-        Fog::Compute::Clodo.new(attributes)
+        Clodo.new(attributes)
       when :ecloud
-        require 'fog/ecloud/compute'
-        Fog::Compute::Ecloud.new(attributes)
+        Ecloud.new(attributes)
       when :glesys
-        require 'fog/glesys/compute'
-        Fog::Compute::Glesys.new(attributes)
+        Glesys.new(attributes)
       when :gogrid
-        require 'fog/go_grid/compute'
-        Fog::Compute::GoGrid.new(attributes)
+        GoGrid.new(attributes)
       when :hp
-        require 'fog/hp/compute'
-        Fog::Compute::HP.new(attributes)
+        HP.new(attributes)
       when :ibm
-        require 'fog/ibm/compute'
-        Fog::Compute::IBM.new(attributes)
+        IBM.new(attributes)
       when :joyent
-        require 'fog/joyent/compute'
-        Fog::Compute::Joyent.new(attributes)
+        Joyent.new(attributes)
       when :libvirt
-        require 'fog/libvirt/compute'
-        Fog::Compute::Libvirt.new(attributes)
+        Libvirt.new(attributes)
       when :linode
-        require 'fog/linode/compute'
-        Fog::Compute::Linode.new(attributes)
+        Linode.new(attributes)
       when :new_servers
-        require 'fog/bare_metal_cloud/compute'
         warn "[DEPRECATION] `new_servers` is deprecated. Please use `bare_metal_cloud` instead."
-        Fog::Compute::BareMetalCloud.new(attributes)
+        BareMetalCloud.new(attributes)
       when :baremetalcloud
-        require 'fog/bare_metal_cloud/compute'
-        Fog::Compute::BareMetalCloud.new(attributes)
+        BareMetalCloud.new(attributes)
       when :ninefold
-        require 'fog/ninefold/compute'
-        Fog::Compute::Ninefold.new(attributes)
+        Ninefold.new(attributes)
       when :openstack
-        require 'fog/openstack/compute'
-        Fog::Compute::OpenStack.new(attributes)
+        OpenStack.new(attributes)
       when :ovirt
-        require 'fog/ovirt/compute'
-        Fog::Compute::Ovirt.new(attributes)
+        Ovirt.new(attributes)
       when :rackspace
-        require 'fog/rackspace/compute'
-        Fog::Compute::Rackspace.new(attributes)
+        Rackspace.new(attributes)
       when :stormondemand
-        require 'fog/storm_on_demand/compute'
-        Fog::Compute::StormOnDemand.new(attributes)
+        StormOnDemand.new(attributes)
       when :vcloud
-        require 'fog/vcloud/compute'
         Fog::Vcloud::Compute.new(attributes)
       when :virtualbox
-        require 'fog/virtual_box/compute'
-        Fog::Compute::VirtualBox.new(attributes)
+        VirtualBox.new(attributes)
       when :vmfusion
-        require 'fog/vmfusion/compute'
-        Fog::Compute::Vmfusion.new(attributes)
+        Vmfusion.new(attributes)
       when :voxel
-        require 'fog/voxel/compute'
-        Fog::Compute::Voxel.new(attributes)
+        Voxel.new(attributes)
       when :vsphere
-        require 'fog/vsphere/compute'
-        Fog::Compute::Vsphere.new(attributes)
+        Vsphere.new(attributes)
       when :xenserver
-        require 'fog/xenserver/compute'
-        Fog::Compute::XenServer.new(attributes)
+        XenServer.new(attributes)
       else
         raise ArgumentError.new("#{provider} is not a recognized compute provider")
       end
